55da4e6898df0103b736c40c4fe16b0ba6d1c6f5
markfarid24 <markfarid2004@gmail.com>
2025-01-31 15:57:57 -0500

    Finished Person class
    finished person class

--- Blackjack/src/Classes/Person.java
+++ Blackjack/src/Classes/Person.java
@@ -1,5 +1,46 @@
-package Classes;
+package Blackjack;
 
 public class Person {
+	private String name;
+	private Hand hand;
+	
+	public Person() {
+		this.name = "";
+		this.hand = new hand();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Hand getHand() {
+		return hand;
+	}
+
+	public void setHand(Hand hand) {
+		this.hand = hand;
+	}
+	
+	public void hit(Deck deck, Deck discard){
+	    if (!deck.hasCards()) {
+	        deck.reloadDeckFromDiscard(discard);
+	    }
+	    this.hand.takeCardFromDeck(deck);
+	    System.out.println(this.name + " gets a new card");
+	    this.getHand();
+	}
+	
+	public boolean has21(){
+	    if(this.getHand().calculatedValue() == 21){
+	        return true;
+	    }
+	    else{
+	        return false;
+	    }
+	}
 
 }
--- Blackjack/src/Classes/Player.java
+++ Blackjack/src/Classes/Player.java
@@ -1,5 +1,46 @@
-package Classes;
+package Blackjack;
 
-public class Player {
+import java.util.Scanner;
 
-}
+public class Player extends Person{
+
+	public Player() {
+		super.setName("PLAYER");
+	}
+	
+	Scanner input = new Scanner(System.in);
+	
+	public void makeDecision(Deck deck, Deck discard) {
+		boolean getNum = true;
+		int decision = 0;
+		
+	    while(getNum){
+	    	try{
+	    		System.out.println("Would you like to (1)hit or (2)stand?");
+	            decision = input.nextInt();
+	            getNum = false;
+
+	            }
+	            catch(Exception e){
+	                System.out.println("Invalid");
+	                input.next();
+	            }
+	    	 if (decision == 1) {
+	             this.hit(deck, discard);
+	             
+	             if(this.getHand().calculatedValue()>21){
+	            	 System.out.println("BUST!");
+	                 return;
+	             }
+	             else{
+	                 this.makeDecision(deck, discard);
+	             }
+	         } 
+	    	 else {
+	             System.out.println("You stand.");
+	         }
+	        }
+	    }
+	}
+
+
