f693a0cdaaa5411158321a56778bff4969674d3b
roopsie <roopkirandhillon1@gmail.com>
2025-02-14 13:55:44 -0500

    User Information for stub databae

---
+++ Blackjack/src/Classes/User.java
@@ -0,0 +1,31 @@
+package Classes;
+
+public class User {
+    private String username;
+    private String password;
+    private int chips;
+    private int wins;
+
+    public User(String username, String password, int chips, int wins) {
+        this.username = username;
+        this.password = password;
+        this.chips = chips;
+        this.wins = wins;
+    }
+
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public int getChips() { return chips; }
+    public int getWins() { return wins; }
+
+    public void setChips(int chips) { this.chips = chips; }
+    public void setWins(int wins) { this.wins = wins; }
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", chips=" + chips +
+                ", wins=" + wins +
+                '}';
+    }
+}
