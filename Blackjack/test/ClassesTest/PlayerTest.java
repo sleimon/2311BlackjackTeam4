b2929d691aa78345682387d990661e5972208057
George <george_zakko@hotmail.com>
2025-02-14 19:51:04 -0500

    So, in the playerTest class, I created another initializer that creates two deck of cards objects, one for actual deck, the other is for discard. Also a new player object.
    Then, I created the first test case, which checks the initial chips amount by using the getter method from the PLayer class.

--- Blackjack/test/ClassesTest/PlayerTest.java
+++ Blackjack/test/ClassesTest/PlayerTest.java
@@ -4,11 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Classes.Player;
+
 class PlayerTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private Player player;
+    private deckOfCards deck;
+    private deckOfCards discard;
+
+
+	@BeforeEach
+	void init() {
+
+		player = new Player(500);
+		deck = new deckOfCards();
+		discard = new deckOfCards();
+
 	}
+
+	@Test
+	void getChipsTest(){
+
+		int expected = 500;
+		int actual = player.getChips();
+
+		assertEquals(expected, actual, "The player should start with 500 chips.  ");
+
+	}
+
+
 
 }
