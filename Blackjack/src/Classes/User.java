71d9bb6f5b8a2997edb74c01d8de830bd63355f3
roopsie <roopkirandhillon1@gmail.com>
2025-02-14 20:49:41 -0500

    Added losses and pushes to store in the database

--- Blackjack/src/Classes/User.java
+++ Blackjack/src/Classes/User.java
@@ -6,27 +6,61 @@ public class User {
     private String password;
     private int chips;
     private int wins;
+    private int losses;
+    private int pushes;
 
-    public User(String username, String password, int chips, int wins) {
+
+    // Constructor to include the data we want to store
+    public User(String username, String password, int chips, int wins, int losses, int pushes) {
         this.username = username;
         this.password = password;
         this.chips = chips;
         this.wins = wins;
+        this.losses = losses;
+        this.pushes = pushes;
     }
-
-    public String getUsername() { return username; }
-    public String getPassword() { return password; }
-    public int getChips() { return chips; }
-    public int getWins() { return wins; }
-
-    public void setChips(int chips) { this.chips = chips; }
-    public void setWins(int wins) { this.wins = wins; }
+// getters and setters
+    public String getUsername() { 
+        return username;
+     }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() { 
+        return password; 
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public int getChips() { 
+        return chips; 
+    }
+    public void setChips(int chips) { 
+        this.chips = chips;
+     }
+    public int getWins() { 
+        return wins; 
+    }
+    public void setWins(int wins){ 
+        this.wins = wins;
+    }
+    public int getLosses(){
+         return losses;
+        }
+    public void setLosses(int losses){ 
+        this.losses = losses;
+    }
+    public int getPushes(){
+        return pushes;
+    }
+    public void setPushes(int pushes){
+        this.pushes =pushes;
+    }
+    
+    
     @Override
     public String toString() {
         return "User{" +
-                "username='" + username + '\'' +
-                ", chips=" + chips +
-                ", wins=" + wins +
-                '}';
+                "username='" + username + '\'' + ", chips=" + chips + ", wins=" + wins + ", losses" + losses +", pushes" +pushes +'}';
     }
 }
