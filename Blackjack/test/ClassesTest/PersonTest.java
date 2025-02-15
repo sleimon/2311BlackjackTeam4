6554a8fcf81accf82778029b25b9c73a7f8c2a2c
George <george_zakko@hotmail.com>
2025-02-14 19:07:17 -0500

    So, I wrote these tests for a previous file that was in the wrong location.  So, I'll reintroduce them again. I have implemented getter and setter tests.  They successfully return name and set the name, respectively. The BeforeEach function creates a new object of person, and deckOfCard, twice (Once for regular deck, other for discard).

--- Blackjack/test/ClassesTest/PersonTest.java
+++ Blackjack/test/ClassesTest/PersonTest.java
@@ -6,9 +6,39 @@ import org.junit.jupiter.api.Test;
 
 class PersonTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	private Person person;
+    private deckOfCards deck;
+    private deckOfCards discard;
+
+    @BeforeEach
+    void init() {
+
+        person = new person();
+        deck = new deckOfCards();
+        discard = new deckOfCards();
+
+    }
+
+    @Test 
+    void getNameTest() {
+
+        string expected = "";
+        string actual = person.getName();
+
+        assertEquals(expected, actual, "Name should be the default value of a string which is nothing.  ");
+
+    }
+
+    @Test 
+    void setNameTest() {
+
+        string expected = "Jeff";
+        person.setName("Jeff");
+        String actual = person.getName();
+        
+        assertEquals(expected, actual, "This name should set to Jeff.  ");
+
+    }
+
 
 }
