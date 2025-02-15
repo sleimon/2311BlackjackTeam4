923c0db124f8a92af22b423a3aa25e4d83ec54ad
George <george_zakko@hotmail.com>
2025-02-14 21:09:22 -0500

    Simply added a name getter test to the Dealer.java class to confirm the dealer's name is DEALER.

--- Blackjack/test/ClassesTest/DealerTest.java
+++ Blackjack/test/ClassesTest/DealerTest.java
@@ -4,11 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Classes.Dealer;
+
 class DealerTest {
 
+	private Dealer dealer;
+
+	@BeforeEach
+	void init(){
+
+		dealer = new Dealer();
+
+	}
+
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void dealerTest() {
+		
+		String expected = "DEALER";
+		String actual = d.getName();
+
+		assertEquals(expected, actual, "The dealer's name should be: DEALER.  ");
+
+
 	}
 
 }
