df2a9aff653e88e038d9109166203289521ce5bb
sleimon <sleimon@my.yorku.ca>
2025-03-28 17:12:43 -0400

    Fixed some ticket #43, code smell ID #14

--- src/main/java/com/blackjack/stubdatabase/StubDatabase.java
+++ src/main/java/com/blackjack/stubdatabase/StubDatabase.java
@@ -97,39 +97,3 @@ public class StubDatabase {
         return user != null && user.getPassword().equals(password);
     }
 }
-
-
-    /*static {
-
-    }
-// retrieving a user by username
-    public static User getUser(String username) {
-        for (User user : users) {
-            if (user.getUsername().equals(username)) {
-                return user;
-            }
-        }
-        return null;
-    }
-// add new user(new Player) to teh stub db without duplicating
-    public static void addUser(User user) {
-        if (getUser(user.getUsername()) == null) { // Prevent duplicates
-            users.add(user);
-    }
-    }
-    // update new player's data in stub db
-    public static void updateUser(User user) {
-        for (int i = 0; i < users.size(); i++) {
-            if (users.get(i).getUsername().equals(user.getUsername())) {
-                users.set(i, user);
-                return;
-            }
-        }
-    }
-     // Print all users (for testing)
-    public static void printUsers() {
-        for (User user : users) {
-            System.out.println(user);
-}
-    }
-}*/
