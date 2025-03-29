af63302c029006466960dc039cfb806c6295396d
roopsie <roopkirandhillon1@gmail.com>
2025-03-28 21:39:48 -0400

    Created a DefaultTableModel with non-editable cells.

--- src/main/java/com/blackjack/Models/Leaderboard.java
+++ src/main/java/com/blackjack/Models/Leaderboard.java
@@ -19,8 +19,14 @@ public class Leaderboard extends JPanel {
 
     }
 
-    public void initLeaderboard(){
+    public void initLeaderboard() {
+        List<User> sortedUsers = UserService.getAllUsers();
+        sortedUsers.sort(Comparator.comparingInt(User::getChips).reversed());
 
+        if (sortedUsers.isEmpty()) {
+            JOptionPane jOptionPane = new JOptionPane();
+            jOptionPane.showMessageDialog(null, "No User available.", "Leaderboard", JOptionPane.INFORMATION_MESSAGE);
+        }
         JFrame frameLB = new JFrame();
         frameLB.setTitle("Blackjack Leaderboard");
         frameLB.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -29,22 +35,25 @@ public class Leaderboard extends JPanel {
 
         String[] columnNames = {"Rank", "Username", "Chips", "Wins", "Losses", "Pushes"};
         //fetching data
-        List<User> sortedUsers = UserService.getAllUsers();
-        sortedUsers.sort(Comparator.comparingInt(User::getChips).reversed());
 
-        String[][] data = new String[Math.min(sortedUsers.size(),10)][6];
-        for(int i=0; i<data.length; i++){
+        String[][] data = new String[Math.min(sortedUsers.size(), 10)][6];
+        for (int i = 0; i < data.length; i++) {
             User user = sortedUsers.get(i);
-            data[i][0]= String.valueOf(i+1);
-            data[i][1]= user.getUsername();
-            data[i][2]= String.valueOf(user.getChips());
-            data[i][3]= String.valueOf(user.getWins());
-            data[i][4]= String.valueOf(user.getLosses());
-            data[i][5]= String.valueOf(user.getPushes());
+            data[i][0] = String.valueOf(i + 1);
+            data[i][1] = user.getUsername();
+            data[i][2] = String.valueOf(user.getChips());
+            data[i][3] = String.valueOf(user.getWins());
+            data[i][4] = String.valueOf(user.getLosses());
+            data[i][5] = String.valueOf(user.getPushes());
         }
 
-        JTable table = new JTable(new DefaultTableModel(data, columnNames));
+        DefaultTableModel model = new DefaultTableModel(data, columnNames){
         //table.setBounds(30, 40, 200, 300);
+        @Override
+        public boolean isCellEditable (int row,int column){
+        return false;
+    }};
+        JTable table = new JTable(model);
         JScrollPane sp = new JScrollPane(table);
         frameLB.add(sp, BorderLayout.CENTER);
         //frameLB.setSize(500, 300);
