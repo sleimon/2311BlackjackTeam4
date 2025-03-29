69a55bc802527c27e16299224f54a795f7978bc4
roopsie <roopkirandhillon1@gmail.com>
2025-03-28 20:27:54 -0400

    fill user data array with details

--- src/main/java/com/blackjack/Models/Leaderboard.java
+++ src/main/java/com/blackjack/Models/Leaderboard.java
@@ -36,7 +36,15 @@ public class Leaderboard extends JPanel {
         sortedUsers.sort(Comparator.comparingInt(User::getChips).reversed());
 
         String[][] data = new String[Math.min(sortedUsers.size(),10)][6];
-
+        for(int i=0; i<data.length; i++){
+            User user = sortedUsers.get(i);
+            data[i][0]= String.valueOf(i+1);
+            data[i][1]= user.getUsername();
+            data[i][2]= String.valueOf(user.getChips());
+            data[i][3]= String.valueOf(user.getWins());
+            data[i][4]= String.valueOf(user.getLosses());
+            data[i][5]= String.valueOf(user.getPushes());
+        }
 
         table = new JTable(data, columnNames);
         table.setBounds(30, 40, 200, 300);
